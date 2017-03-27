b85db20964c857ef8f399dec78eac757aee59990
Mohammed Irfan <irfan@hamon.in>
2017-03-27 16:56:35 +0530

    Add usrsctp and libnice modules + build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+cmake -DLIBNICE_LIBRARY=./libnice.so -DUSRSCTP_LIBRARY=./libusrsctp.so -DLIBNICE_INCLUDE_DIR="./libnice/nice;./libnice/agent;./libnice" -DUSRSCTP_INCLUDE_DIR=./usrsctp/usrsctplib -DDISABLE_SPD_LOG=True
