bd36573c7dfaf18d31aa6ec6640a408914e9b63a
Mohammed Irfan <irfan@hamon.in>
2017-03-28 13:53:57 +0530

    Add make in build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-cmake -DLIBNICE_LIBRARY=./libnice.so -DUSRSCTP_LIBRARY=./libusrsctp.so -DLIBNICE_INCLUDE_DIR="./libnice/nice;./libnice/agent;./libnice" -DUSRSCTP_INCLUDE_DIR=./usrsctp/usrsctplib -DDISABLE_SPD_LOG=True
+cmake -DLIBNICE_LIBRARY=./libnice.so -DUSRSCTP_LIBRARY=./libusrsctp.so -DLIBNICE_INCLUDE_DIR="./libnice/nice/;./libnice/agent/;./libnice/;" -DUSRSCTP_INCLUDE_DIR=./usrsctp/usrsctplib -DDISABLE_SPD_LOG=True
+make
