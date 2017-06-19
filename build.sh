cfd385d03f43b795642751c25c39eb1fad336ea1
Hasna A <hasna@hamon.in>
2017-06-19 11:30:37 +0530

    Add make for examples/websocket_client/cpslib/ in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+cd examples/websocket_client/cpslib/
+make
+cd ../../../
 cmake -DUSRSCTP_LIBRARY=./usrsctp/usrsctplib/.libs/libusrsctp.so.1.0.0 -DUSRSCTP_INCLUDE_DIR=./usrsctp/usrsctplib -DSPDLOG_INCLUDE_DIR="./spdlog/include/" -DDISABLE_SPDLOG=off -DCMAKE_BUILD_TYPE=Debug
 make
