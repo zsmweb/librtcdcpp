5ff4a39bae3afce200c4de7e016cc0c99744b40f
Irfan <irfan@hamon.in>
2017-06-21 14:56:41 +0530

    Write the C wrapper

--- include/rtcdcpp/librtcdcpp.h
+++ include/rtcdcpp/librtcdcpp.h
@@ -1,30 +1,82 @@
 /**
- * Copyright (c) 2017, Andrew Gault, Nick Chadwick and Guillaume Egles.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of the <organization> nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
  * C Wrapper around the C++ Classes.
  */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+typedef struct rtcdcpp::PeerConnection PeerConnection;
+typedef struct rtcdcpp::DataChannel DataChannel;
+#else
+typedef struct PeerConnection PeerConnection;
+typedef struct DataChannel DataChannel;
+#endif
+
+#include <stdbool.h>
+#include <glib.h>
+#include <sys/types.h>
+
+struct RTCIceServer_C {
+  const char* hostname;
+  int port;
+};
+
+struct RTCConfiguration_C {
+  const GArray* ice_servers; // RTCIceServer_C
+  unsigned int ice_port_range1;
+  unsigned int ice_port_range2;
+  const char* ice_ufrag;
+  const char* ice_pwd;
+  const GArray* certificates; // RTCCertificate
+};
+struct IceCandidate_C {
+    const char* candidate;
+    const char* sdpMid;
+    int sdpMLineIndex;
+};
+typedef struct IceCandidate_C IceCandidate_C;
+IceCandidate_C* newIceCandidate(const char* candidate, const char* sdpMid, int sdpMLineIndex);
+
+typedef void (*on_ice_cb)(IceCandidate_C ice_c);
+typedef void (*on_dc_cb)(DataChannel *dc);
+PeerConnection* newPeerConnection(struct RTCConfiguration_C config, on_ice_cb ice_cb, on_dc_cb dc_cb);
+void destroyPeerConnection(PeerConnection* pc);
+
+void ParseOffer(PeerConnection* pc, const char* sdp);
+const char* GenerateOffer(PeerConnection* pc);
+const char* GenerateAnswer(PeerConnection* pc);
+
+bool SetRemoteIceCandidate(PeerConnection* pc, const char* candidate_sdp); 
+bool SetRemoteIceCandidates(PeerConnection* pc, const GArray* candidate_sdps);
+
+DataChannel *CreateDataChannel(PeerConnection* pc, const char* label, const char* protocol);
+
+// DataChannel member functions
+u_int16_t getDataChannelStreamID(DataChannel *dc);
+u_int8_t getDataChannelType(DataChannel *dc);
+const char* getDataChannelLabel(DataChannel *dc);
+const char* getDataChannelProtocol(DataChannel *dc);
+
+bool SendString(DataChannel *dc, const char* msg);
+bool SendBinary(DataChannel *dc, const u_int8_t *msg, int len);
+
+void closeDataChannel(DataChannel *dc);
+
+//DataChannel Callback related methods
+typedef void (*open_cb)(void);
+typedef void (*on_string_msg)(const char* message);
+typedef void (*on_string_msg)(const char* message);
+typedef void (*on_binary_msg)(void* message);
+typedef void (*on_close)(void);
+typedef void (*on_error)(const char* description);
+
+//DataChannel Callback setters
+void SetOnOpen(DataChannel *dc, open_cb on_open_cb);
+void SetOnStringMsgCallback(DataChannel *dc, on_string_msg recv_str_cb);
+void SetOnBinaryMsgCallback(DataChannel *dc, on_binary_msg msg_binary_cb);
+void SetOnClosedCallback(DataChannel *dc, on_close close_cb);
+void SetOnErrorCallback(DataChannel *dc, on_error error_cb);
+#ifdef __cplusplus
+}
+#endif
---
+++ src/librtcdcpp.cpp
@@ -0,0 +1,175 @@
+/**
+ * C Wrapper around the C++ classes.
+ */
+
+#include <rtcdcpp/PeerConnection.hpp>
+#include <rtcdcpp/librtcdcpp.h> //hpp?
+#include <rtcdcpp/DataChannel.hpp>
+#include <glib.h>
+#include <stdbool.h>
+#include <memory>
+#include <functional>
+//using namespace rtcdcpp;
+#include <iostream>
+#include <sys/types.h>
+
+extern "C" {
+  IceCandidate_C* newIceCandidate(const char* candidate, const char* sdpMid, int sdpMLineIndex) {
+    //IceCandidate_C ice_cand;
+    IceCandidate_C* ice_cand = (IceCandidate_C *) malloc(sizeof(IceCandidate_C));
+    ice_cand->candidate = candidate; //&
+    ice_cand->sdpMid = sdpMid;
+    ice_cand->sdpMLineIndex = sdpMLineIndex;
+    return ice_cand;
+  }
+
+  PeerConnection* newPeerConnection(RTCConfiguration_C config_c, on_ice_cb ice_cb, on_dc_cb dc_cb) {
+    
+    std::function<void(rtcdcpp::PeerConnection::IceCandidate)> onLocalIceCandidate = [ice_cb](rtcdcpp::PeerConnection::IceCandidate candidate) 
+    {
+      IceCandidate_C ice_cand_c;
+      ice_cand_c.candidate = candidate.candidate.c_str(); //
+      ice_cand_c.sdpMid = candidate.sdpMid.c_str();
+      ice_cand_c.sdpMLineIndex = candidate.sdpMLineIndex;
+      ice_cb(ice_cand_c);
+    };
+
+    std::function<void(std::shared_ptr<rtcdcpp::DataChannel> channel)> onDataChannel = [dc_cb](std::shared_ptr<rtcdcpp::DataChannel> channel) {
+      dc_cb((DataChannel*) channel.get());
+    };
+    rtcdcpp::RTCConfiguration config;
+    for(int i = 0; i < config_c.ice_servers->len; i++) {
+      config.ice_servers.emplace_back(rtcdcpp::RTCIceServer{"stun3.l.google.com", 19302});
+    }
+    std::pair<unsigned, unsigned> port_range = std::make_pair(config_c.ice_port_range1, config_c.ice_port_range2);
+    config.ice_port_range = port_range;
+    
+    if (config_c.ice_ufrag) {
+      std::string ice_ufrag_string (config_c.ice_ufrag);
+      config.ice_ufrag = ice_ufrag_string;
+    }
+    if (config_c.ice_pwd) {
+      std::string ice_pwd_string (config_c.ice_pwd);
+      config.ice_pwd = ice_pwd_string;
+    }
+    //TODO: Fix this
+    /*
+    for(int i = 0; i <= config_c.certificates->len; i++) {
+      rtcdcpp::RTCCertificate *rtc_cert;
+      rtc_cert = &g_array_index(config_c.certificates, rtcdcpp::RTCCertificate, i);
+      config.certificates.emplace_back(&rtc_cert);
+    }
+    */
+    return (PeerConnection*) new rtcdcpp::PeerConnection(config, onLocalIceCandidate, onDataChannel);
+  }
+  
+  void destroyPeerConnection(PeerConnection *pc) {
+    delete pc;
+  }
+
+  void ParseOffer(PeerConnection* pc, const char* sdp) {
+    std::string sdp_string (sdp);
+    pc->ParseOffer(sdp_string);
+  }
+
+  const char* GenerateOffer(PeerConnection *pc) {
+    std::string ret_val;
+    ret_val = pc->GenerateOffer();
+    return ret_val.c_str(); //
+  }
+
+  const char* GenerateAnswer(PeerConnection *pc) {
+    std::string ret_val;
+    ret_val = pc->GenerateAnswer();
+    return ret_val.c_str();
+  }
+
+  bool SetRemoteIceCandidate(PeerConnection *pc, const char* candidate_sdp) {
+    std::string candidate_sdp_string (candidate_sdp);
+    return pc->SetRemoteIceCandidate(candidate_sdp_string);
+  }
+
+  bool SetRemoteIceCandidates(PeerConnection *pc, const GArray* candidate_sdps) {
+    std::vector<std::string> candidate_sdps_vec;
+    for(int i = 0; i <= candidate_sdps->len; i++) {
+      std::string candidate_sdp_string (&g_array_index(candidate_sdps, char, i));
+      candidate_sdps_vec.emplace_back(candidate_sdp_string);
+    }
+    return pc->SetRemoteIceCandidates(candidate_sdps_vec);
+  }
+
+  DataChannel* CreateDataChannel(PeerConnection *pc, const char* label, const char* protocol) {
+    std::string label_string (label);
+    std::string protocol_string (protocol);
+    if (protocol_string.size() > 0) {
+      return pc->CreateDataChannel(label, protocol).get();
+    } else {
+      return pc->CreateDataChannel(label).get();
+    }
+  }
+
+  u_int16_t getDataChannelStreamID(DataChannel *dc) {
+    return dc->GetStreamID();
+  }
+
+  u_int8_t getDataChannelType(DataChannel *dc) {
+    return dc->GetChannelType();
+  }
+
+  const char* getDataChannelLabel(DataChannel *dc) {
+    std::string dc_label_string; 
+    dc_label_string = dc->GetLabel();
+    return dc_label_string.c_str();
+  }
+
+  const char* getDataChannelProtocol(DataChannel *dc) {
+    std::string dc_proto_string;
+    dc_proto_string = dc->GetProtocol();
+    return dc_proto_string.c_str();
+  }
+
+  bool SendString(DataChannel *dc, const char* msg) {
+    std::string message (msg);
+    return dc->SendString(message);
+  }
+
+  bool SendBinary(DataChannel *dc, const u_int8_t *msg, int len) {
+    return dc->SendBinary(msg, len);
+  }
+  
+  void closeDataChannel(DataChannel *dc) {
+    delete dc;
+  }
+
+  void SetOnOpen(DataChannel *dc, open_cb on_open_cb) {
+    dc->SetOnOpen(on_open_cb);
+  }
+
+  void SetOnStringMsgCallback(DataChannel *dc, on_string_msg recv_str_cb) {
+    std::function<void(std::string string1)> recv_callback = [recv_str_cb](std::string string1) {
+      recv_str_cb(string1.c_str());
+    };
+    dc->SetOnStringMsgCallback(recv_callback);
+  }
+
+  void SetOnBinaryMsgCallback(DataChannel *dc, on_binary_msg msg_binary_cb) {
+    std::function<void(rtcdcpp::ChunkPtr)> msg_binary_callback = [msg_binary_cb](rtcdcpp::ChunkPtr chkptr) {
+      msg_binary_cb((void *) chkptr.get()->Data());
+    };
+    dc->SetOnBinaryMsgCallback(msg_binary_callback);
+  }
+
+  void SetOnClosedCallback(DataChannel *dc, on_close close_cb) {
+    std::function<void()> close_callback = [close_cb]() {
+      close_cb();
+    };
+    dc->SetOnClosedCallback(close_callback);
+  }
+
+  void SetOnErrorCallback(DataChannel *dc, on_error error_cb) {
+    std::function<void(std::string description)> error_callback = [error_cb](std::string description) {
+      error_cb(description.c_str());
+    };
+    dc->SetOnErrorCallback(error_callback);
+  }
+}
