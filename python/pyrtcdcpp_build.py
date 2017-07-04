a962fa8692a955d41f5895fff46267763ba6deba
Irfan <irfan@hamon.in>
2017-07-04 12:46:20 +0530

    Include the Python CFFI scripts

---
+++ python/pyrtcdcpp_build.py
@@ -0,0 +1,59 @@
+from cffi import FFI
+import os
+
+ffibuilder = FFI()
+ffibuilder.set_source("libpyrtcdcpp",
+        """
+        typedef struct PeerConnection PeerConnection;
+        typedef struct DataChannel DataChannel;
+        #include <stdbool.h>
+        #include <glib.h>
+        #include <sys/types.h>
+        #include "rtcdcpp/librtcdcpp.h"
+        """,
+        include_dirs = ["../src/", "../include", "/usr/include/glib-2.0", "/usr/lib64/glib-2.0/include"],
+        libraries = ["rtcdcpp"],
+        library_dirs = ["../"])
+
+lines = open(os.path.abspath("../include/rtcdcpp") + "/librtcdcpp.h")
+altered_lines = ['' if line.startswith('#include ') or line.startswith('#pragma') or line.startswith('extern') or line.startswith('typedef struct rtcdcpp::') or line.startswith('#if') or line.startswith('#e') else line for line in lines]
+
+cdef_string = '''
+typedef char... gchar;
+typedef unsigned int guint; 
+struct GArray {
+    gchar *data; 
+    guint len;
+};
+typedef struct GArray GArray;
+typedef int16_t u_int16_t;
+typedef int8_t u_int8_t;
+typedef bool gboolean;
+typedef const void *gconstpointer;
+
+GArray *
+g_array_new (gboolean zero_terminated,
+             gboolean clear_,
+             guint element_size);
+GArray * g_array_append_vals(GArray* a, gconstpointer v, guint len);
+''' + ''.join(altered_lines)
+
+cdef_string = cdef_string[:-2] # Remove the trailing }
+
+cdef_string += '''
+extern "Python" void onDCCallback(DataChannel *dc);
+extern "Python" void onIceCallback(IceCandidate_C ice);
+
+extern "Python" void onOpen(void);
+extern "Python" void onClose(void);
+extern "Python" void onStringMsg(const char* message);
+extern "Python" void onBinaryMsg(void* message);
+extern "Python" void onError(const char* description);
+
+'''
+
+ffibuilder.cdef(cdef_string)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose = True)
