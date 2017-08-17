458d8f85427fd951107d6ebb196d48c7c742961c
Irfan <irfan@hamon.in>
2017-08-17 15:43:48 +0530

    Autoformat PeerConnection.hpp
    Made diff-ing totally useless, sorry about that.
    I don't even remember what I changed :(

--- include/rtcdcpp/PeerConnection.hpp
+++ include/rtcdcpp/PeerConnection.hpp
@@ -36,133 +36,133 @@
 
 namespace rtcdcpp {
 
-class NiceWrapper;
-class DTLSWrapper;
-class SCTPWrapper;
+  class NiceWrapper;
+  class DTLSWrapper;
+  class SCTPWrapper;
 
-struct RTCIceServer {
-  std::string hostname;
-  int port;
-};
-
-std::ostream &operator<<(std::ostream &os, const RTCIceServer &ice_server);
-
-struct RTCConfiguration {
-  std::vector<RTCIceServer> ice_servers;
-  std::pair<unsigned, unsigned> ice_port_range;
-  std::string ice_ufrag;
-  std::string ice_pwd;
-  std::vector<RTCCertificate> certificates;
-};
-
-class PeerConnection {
-  friend class DTLSWrapper;
-  friend class DataChannel;
- public:
-  struct IceCandidate {
-    IceCandidate(const std::string &candidate, const std::string &sdpMid, int sdpMLineIndex)
-        : candidate(candidate), sdpMid(sdpMid), sdpMLineIndex(sdpMLineIndex) {}
-    std::string candidate;
-    std::string sdpMid;
-    int sdpMLineIndex;
+  struct RTCIceServer {
+    std::string hostname;
+    int port;
   };
 
-  using IceCandidateCallbackPtr = std::function<void(IceCandidate)>;
-  using DataChannelCallbackPtr = std::function<void(std::shared_ptr<DataChannel> channel)>;
+  std::ostream &operator<<(std::ostream &os, const RTCIceServer &ice_server);
 
-  PeerConnection(const RTCConfiguration &config, IceCandidateCallbackPtr icCB, DataChannelCallbackPtr dcCB);
+  struct RTCConfiguration {
+    std::vector<RTCIceServer> ice_servers;
+    std::pair<unsigned, unsigned> ice_port_range;
+    std::string ice_ufrag;
+    std::string ice_pwd;
+    std::vector<RTCCertificate> certificates;
+  };
 
-  virtual ~PeerConnection();
+  class PeerConnection {
+    friend class DTLSWrapper;
+    friend class DataChannel;
+    public:
+    struct IceCandidate {
+      IceCandidate(const std::string &candidate, const std::string &sdpMid, int sdpMLineIndex)
+        : candidate(candidate), sdpMid(sdpMid), sdpMLineIndex(sdpMLineIndex) {}
+      std::string candidate;
+      std::string sdpMid;
+      int sdpMLineIndex;
+    };
 
-  const RTCConfiguration &config() { return config_; }
+    using IceCandidateCallbackPtr = std::function<void(IceCandidate)>;
+    using DataChannelCallbackPtr = std::function<void(std::shared_ptr<DataChannel> channel, void *socket)>;
 
-  /**
-   *
-   * Parse Offer SDP
-   */
-  void ParseOffer(std::string offer_sdp);
+    PeerConnection(const RTCConfiguration &config, IceCandidateCallbackPtr icCB, DataChannelCallbackPtr dcCB, void *socket);
 
-  /**
-   * Generate Offer SDP
-   */
-  std::string GenerateOffer();
+    virtual ~PeerConnection();
 
-  /**
-   * Generate Answer SDP
-   */
-  std::string GenerateAnswer();
+    const RTCConfiguration &config() { return config_; }
 
-  /**
-  * Handle remote ICE Candidate.
-  * Supports trickle ice candidates.
-  */
-  bool SetRemoteIceCandidate(std::string candidate_sdp);
+    /**
+     *
+     * Parse Offer SDP
+     */
+    void ParseOffer(std::string offer_sdp);
 
-  /**
-  * Handle remote ICE Candidates.
-  * TODO: Handle trickle ice candidates.
-  */
-  bool SetRemoteIceCandidates(std::vector<std::string> candidate_sdps);
+    /**
+     * Generate Offer SDP
+     */
+    std::string GenerateOffer();
 
-  /**
-   * Create a new data channel with the given label.
-   * Only callable once RTCConnectedCallback has been called.
-   * TODO: Handle creating data channels before generating SDP, so that the
-   *       data channel is created as part of the connection process.
-   */
-  std::shared_ptr<DataChannel> CreateDataChannel(std::string label, std::string protocol="");
+    /**
+     * Generate Answer SDP
+     */
+    std::string GenerateAnswer();
 
-  /**
-   * Notify when remote party creates a DataChannel.
-   * XXX: This is *not* a callback saying that a call to CreateDataChannel
-   *      has succeeded. This is a call saying the remote party wants to
-   *      create a new data channel.
-   */
-  //	void SetDataChannelCreatedCallback(DataChannelCallbackPtr cb);
+    /**
+     * Handle remote ICE Candidate.
+     * Supports trickle ice candidates.
+     */
+    bool SetRemoteIceCandidate(std::string candidate_sdp);
 
-  // TODO: Error callbacks
+    /**
+     * Handle remote ICE Candidates.
+     * TODO: Handle trickle ice candidates.
+     */
+    bool SetRemoteIceCandidates(std::vector<std::string> candidate_sdps);
 
-  void SendStrMsg(std::string msg, uint16_t sid);
-  void SendBinaryMsg(const uint8_t *data, int len, uint16_t sid);
+    /**
+     * Create a new data channel with the given label.
+     * Only callable once RTCConnectedCallback has been called.
+     * TODO: Handle creating data channels before generating SDP, so that the
+     *       data channel is created as part of the connection process.
+     */
+    std::shared_ptr<DataChannel> CreateDataChannel(std::string label, std::string protocol="");
 
-  /* Internal Callback Handlers */
-  void OnLocalIceCandidate(std::string &ice_candidate);
-  void OnIceReady();
-  void OnDTLSHandshakeDone();
-  void OnSCTPMsgReceived(ChunkPtr chunk, uint16_t sid, uint32_t ppid);
+    /**
+     * Notify when remote party creates a DataChannel.
+     * XXX: This is *not* a callback saying that a call to CreateDataChannel
+     *      has succeeded. This is a call saying the remote party wants to
+     *      create a new data channel.
+     */
+    //  void SetDataChannelCreatedCallback(DataChannelCallbackPtr cb);
 
- private:
-  RTCConfiguration config_;
-  const IceCandidateCallbackPtr ice_candidate_cb;
-  const DataChannelCallbackPtr new_channel_cb;
+    // TODO: Error callbacks
 
-  std::string mid;
+    void SendStrMsg(std::string msg, uint16_t sid);
+    void SendBinaryMsg(const uint8_t *data, int len, uint16_t sid);
 
-  enum Role { Client, Server } role = Client;
+    /* Internal Callback Handlers */
+    void OnLocalIceCandidate(std::string &ice_candidate);
+    void OnIceReady();
+    void OnDTLSHandshakeDone();
+    void OnSCTPMsgReceived(ChunkPtr chunk, uint16_t sid, uint32_t ppid);
 
-  std::atomic<bool> iceReady{false};
-  std::unique_ptr<NiceWrapper> nice;
-  std::unique_ptr<DTLSWrapper> dtls;
-  std::unique_ptr<SCTPWrapper> sctp;
+    private:
+    RTCConfiguration config_;
+    const IceCandidateCallbackPtr ice_candidate_cb;
+    const DataChannelCallbackPtr new_channel_cb;
+    void *socket;
+    std::string mid;
 
-  std::map<uint16_t, std::shared_ptr<DataChannel>> data_channels;
-  std::shared_ptr<DataChannel> GetChannel(uint16_t sid);
+    enum Role { Client, Server } role = Client;
 
-  /**
-  * Constructor helper
-  * Initialize the RTC connection.
-  * Allocates all internal structures and configs, and starts ICE gathering.
-  */
-  bool Initialize();
+    std::atomic<bool> iceReady{false};
+    std::unique_ptr<NiceWrapper> nice;
+    std::unique_ptr<DTLSWrapper> dtls;
+    std::unique_ptr<SCTPWrapper> sctp;
 
-  // DataChannel message parsing
-  void HandleNewDataChannel(ChunkPtr chunk, uint16_t sid);
-  void HandleDataChannelAck(uint16_t sid);
-  void HandleDataChannelClose(uint16_t sid);
-  void HandleStringMessage(ChunkPtr chunk, uint16_t sid);
-  void HandleBinaryMessage(ChunkPtr chunk, uint16_t sid);
+    std::map<uint16_t, std::shared_ptr<DataChannel>> data_channels;
+    std::shared_ptr<DataChannel> GetChannel(uint16_t sid);
 
-  std::shared_ptr<Logger> logger = GetLogger("rtcdcpp.PeerConnection");
-  void ResetSCTPStream(uint16_t stream_id);
-};
+    /**
+     * Constructor helper
+     * Initialize the RTC connection.
+     * Allocates all internal structures and configs, and starts ICE gathering.
+     */
+    bool Initialize();
+
+    // DataChannel message parsing
+    void HandleNewDataChannel(ChunkPtr chunk, uint16_t sid);
+    void HandleDataChannelAck(uint16_t sid);
+    void HandleDataChannelClose(uint16_t sid);
+    void HandleStringMessage(ChunkPtr chunk, uint16_t sid);
+    void HandleBinaryMessage(ChunkPtr chunk, uint16_t sid);
+
+    std::shared_ptr<Logger> logger = GetLogger("rtcdcpp.PeerConnection");
+    void ResetSCTPStream(uint16_t stream_id);
+  };
 }
